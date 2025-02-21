03a10439d857ac33eee8855041166f378b2df017
Aditya Adavade <adityawork118@gmail.com>
2025-02-21 17:11:29 +0530

    Add files via upload
    Uploaded Files to repo

---
+++ password_manager/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Manager from "./components/Manager";
+
+const App = () => {
+  return (
+    <>
+      <div className="w-[75vw] mx-auto max-sm:w-[90vw]">
+        <Manager />
+      </div>
+    </>
+  );
+};
+
+export default App;
