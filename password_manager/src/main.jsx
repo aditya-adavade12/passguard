70bae6a6b53f1fba5e610ec605e95d6d6cd59ccd
Aditya Adavade <adityawork118@gmail.com>
2025-02-21 17:18:42 +0530

    Deleted password_manager directory

--- password_manager/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import { createRoot } from 'react-dom/client'
-import React from 'react'
-import './index.css'
-import App from './App.jsx'
-const secretKey = import.meta.env.VITE_SECRET_KEY;
-
-createRoot(document.getElementById('root')).render(
-    <App />
-)
